f64422794643f9a378c3bb86c3e74b32633fcb35
Alex72005 <asm0125@alu.medac.es>
2025-04-09 11:09:19 +0200

    cronometro

--- tercer_trimestre/tema18-19/ej6/script.js
+++ tercer_trimestre/tema18-19/ej6/script.js
@@ -1,12 +1,17 @@
 let intervalo;
 let tiempo = 0;
 
+const tiempoElemento = document.getElementById("tiempo");
+const botonIniciar = document.getElementById("iniciar");
+const botonParar = document.getElementById("parar");
+const botonResetear = document.getElementById("resetear");
+
 function actualizarTiempo() {
   let horas = String(Math.floor(tiempo / 3600)).padStart(2, '0');
   let minutos = String(Math.floor((tiempo % 3600) / 60)).padStart(2, '0');
   let segundos = String(tiempo % 60).padStart(2, '0');
   
-  document.getElementById("tiempo").textContent = `${horas}:${minutos}:${segundos}`;
+  tiempoElemento.textContent = horas + ":" + minutos + ":" + segundos;
 }
 
 function iniciarCronometro() {
@@ -29,3 +34,8 @@ function resetearCronometro() {
   tiempo = 0;
   actualizarTiempo();
 }
+
+botonIniciar.addEventListener("click",iniciarCronometro);
+botonParar.addEventListener("click",pararCronometro);
+botonResetear.addEventListener("click",resetearCronometro);
+
